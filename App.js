025451a18f027cb7473f5edeb575d902501820c6
ChristianPlesca <cristiplesca1993@gmail.com>
2021-01-19 00:35:26 +0000

    TabNavigator & Icons implemented

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    const Stack = createStackNavigator();
+    const Tab = createBottomTabNavigator();
+
+    const MainScreenTabNav = () => (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color }) => {
+            let iconName;
+            if (route.name === 'Map') {
+              iconName = focused
+                ? 'ios-map'
+                : 'ios-map-outline';
+            } else if (route.name === 'Deck') {
+                iconName = focused ? 'albums' : 'albums-outline';
+            } else if (route.name === 'Review') {
+              iconName = focused ? 'documents' : 'documents-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={30} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Deck" component={DeckScreen} />
+          <Tab.Screen name="Review" component={ReviewScreen} />
+        </Tab.Navigator>
+      );
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{ title: 'Welcome Screen' }}
+          />
+          <Stack.Screen name="Auth" component={AuthScreen} />
+          <Stack.Screen name="TabNav" component={MainScreenTabNav} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
 
 class WelcomeScreen extends Component {
     render() {
-        return (
-            <View>
-                <Text>WelcomeScreen</Text>
-            </View>
-        );
+      return (
+        <View style={{ marginTop: 100 }}>
+          <Button 
+            title="Navigate" 
+            onPress={() => this.props.navigation.navigate('TabNav', { screen: 'Home' })} 
+          />
+        </View>
+    );
     }
 }
 
